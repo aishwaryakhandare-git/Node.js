185e6ce82d53e41140f8185423a1b6ff06591fe6
Aishwarya Khandare <aishwaryakhandare005@gmail.com>
2026-07-19 16:34:22 +0530

    CRM Backend Checks

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const app = express();
+app.use(express.json());
 app.listen(3000, () =>{
-    console.log("Server runnign on port 3000")
+    console.log("Server running on port 3000")
+})
+
+app.get("/",(req,res)=>{
+    res.send("CRM Backend Running")
 })
